6c7d58ae017e68b936f4b794b1cb817106fe037a
Al Deniro Da Silva <dasilvaaldeniro0@gmail.com>
2026-05-11 20:01:50 +0200

    Fix static files

--- library_project/settings.py
+++ library_project/settings.py
@@ -28,6 +28,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -88,10 +90,6 @@ STATICFILES_DIRS = [
     BASE_DIR / 'library_app/static',
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = 'media/'
-
-MEDIA_ROOT = BASE_DIR / 'media'
-
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
